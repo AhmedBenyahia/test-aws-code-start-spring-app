bd8628e79b4080bdd41227f78111c4040a9cb3b1
Ahmed Benyahia <ahmed.benyahia@dqlick.com>
2021-08-15 10:35:25 +0100

    Test CI/CD

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -17,6 +17,6 @@ public class HelloWorldHandler implements RequestHandler<Object, Object> {
     public Object handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        return new GatewayResponse(new JSONObject().put("Output", "Hello World!").toString(), headers, 200);
+        return new GatewayResponse(new JSONObject().put("Output", "Hello World from sayto !").toString(), headers, 200);
     }
 }
